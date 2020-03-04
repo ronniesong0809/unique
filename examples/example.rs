a15a0198d5598a9a9a319a67699db4b6dd5b146c
Ronnie Song <ronniesong0809@gmail.com>
2020-03-04 15:32:12 -0800

    refactor: try more examples

--- examples/example.rs
+++ examples/example.rs
@@ -1,15 +1,43 @@
 use unique::Unique;
 
 pub fn main() {
+    unique_num();
+    unique_name();
+}
+
+fn unique_num() {
     let mut nums = vec![];
     let even: fn(&&usize) -> bool = |&&n| n % 2 == 0;
-    assert_eq!(None, nums.iter().unique(even));
+    let odd: fn(&&usize) -> bool = |&&n| n % 2 != 0;
+    let find_3: fn(&&usize) -> bool = |&&n| n == 3;
+
+    println!("{:?}", nums.iter().unique(even));
+    println!("{:?}", nums.iter().unique(odd));
     nums.push(1);
-    assert_eq!(None, nums.iter().unique(even));
     nums.push(0);
-    assert_eq!(Some(&0), nums.iter().unique(even));
-    nums.push(3);
+    println!("{:?}", nums.iter().unique(even));
+    println!("{:?}", nums.iter().unique(odd));
     nums.push(2);
     nums.push(5);
-    assert_eq!(None, nums.iter().unique(even));
+    println!("{:?}", nums.iter().unique(even));
+    println!("{:?}", nums.iter().unique(odd));
+    nums.push(3);
+    println!("{:?}", nums.iter().unique(find_3));
+    nums.push(3);
+    println!("{:?}", nums.iter().unique(find_3));
+}
+
+fn unique_name() {
+    let mut names = vec![];
+    let find_ronnie: fn(&&&str) -> bool = |&&n| n == "Ronnie";
+    let find_david: fn(&&&str) -> bool = |&&n| n == "David";
+
+    names.push("Ronnie");
+    names.push("John");
+    names.push("David");
+    println!("{:?}", names.iter().unique(find_ronnie));
+    println!("{:?}", names.iter().unique(find_david));
+    names.push("David");
+    println!("{:?}", names.iter().unique(find_ronnie));
+    println!("{:?}", names.iter().unique(find_david));
 }
