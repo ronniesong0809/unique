7d10b446f4cf143e92189ffc58f7a86c3ef54f57
Ronnie Song <ronniesong0809@gmail.com>
2020-03-04 15:16:42 -0800

    feat: finish unique crate

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,16 @@ impl<T, I> Unique<T> for I
 where
     I: Iterator<Item = T>,
 {
-    fn unique<P>(&mut self, _pred: P) -> Option<T> {
-        None
+    fn unique<P>(&mut self, mut pred: P) -> Option<T>
+    where
+        P: FnMut(&T) -> bool,
+    {
+        let mut val = self.filter(|x| pred(x));
+        let value = val.next();
+        let value_next = val.next();
+        match value_next {
+            Some(_) => None,
+            None => value,
+        }
     }
 }
---
+++ tests/test.rs
@@ -0,0 +1,16 @@
+use unique::Unique;
+
+#[test]
+pub fn test() {
+    let mut nums = vec![];
+    let even: fn(&&usize) -> bool = |&&n| n % 2 == 0;
+    assert_eq!(None, nums.iter().unique(even));
+    nums.push(1);
+    assert_eq!(None, nums.iter().unique(even));
+    nums.push(0);
+    assert_eq!(Some(&0), nums.iter().unique(even));
+    nums.push(3);
+    nums.push(2);
+    nums.push(5);
+    assert_eq!(None, nums.iter().unique(even));
+}
